dc90151346bae90ae98024885b267a0e19c3b3a6
Fanque <48665454+roshan38@users.noreply.github.com>
2019-08-13 21:38:45 +0800

    Update linkedListCRUD.go

--- 数据结构与算法/06.LinkedList01/linkedListCRUD/linkedListCRUD.go
+++ 数据结构与算法/06.LinkedList01/linkedListCRUD/linkedListCRUD.go
@@ -1,1 +1,27 @@
-123
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+type linkedList struct {
+	Data int
+	Next *linkedList
+}
+
+func main {
+	var p *linkedList
+	p.Data = 1
+	h := p
+	for a := 1; a < 20; a++ {
+		p.Data = a
+		p = p.Next
+	}
+	p = nil
+	fmt.Println(*h)
+}
+
+func getData(Data int) i int{
+     
+}
